f34f4a5e3c649e6a50d7824d4e5810ae478fe262
Thomas Peklak <thomas.peklak@gmail.com>
2013-06-24 21:01:46 +0200

    always return a new scuttlebutt model

--- client.js
+++ client.js
@@ -4,11 +4,11 @@ var seaport = require("seaport");
 var version = require("./package").version;
 var net = require("net");
 var Scuttlebutt = require("scuttlebutt/model");
-var config = new Scuttlebutt();
 
 module.exports = function (host, port) {
     var ports = seaport.connect(host, port);
 
+    var config = new Scuttlebutt();
     ports.get("registry@" + version, function (ps) {
         var registryStream = net.connect(ps[0]);
         registryStream.pipe(config.createStream()).pipe(registryStream);
